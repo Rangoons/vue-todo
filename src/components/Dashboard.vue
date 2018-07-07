7db63f61fa3be7060efffb507a240413cce9b010
Brendan McDonald <brendan.mcdonald@eagledream.com>
2018-07-07 13:52:03 -0400

    dashboard is properly passing props to todoitem

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,21 +1,23 @@
-<template>
-  <TodoItem v-for="(todo, index) in todos" :key="index" :todoName="todo.todoName" :checked="todo.checked" />
+<template >
+  <div> 
+    <TodoItem v-for="(todo) in todos" :key="todo.id" :todoName="todo.todoName" :checked="todo.checked" />
+  </div>
 </template>
 
 <script>
 import todos from '../../mock-data/mock.js';
-import TodoItem from './TodoItem.vue'
+import TodoItem from './TodoItem.vue';
 
 export default {
-  name: 'Dashboard',
-  components: {
-    TodoItem
-  },
-  data(){
-      return {
-          todos
-      }
-  }
-}
+    name: 'Dashboard',
+    components: {
+        TodoItem,
+    },
+    data() {
+        return {
+            todos,
+        };
+    },
+};
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-// import KeenUI from 'keen-ui';
-// import 'keen-ui/dist/keen-ui.css';
+import Vue from 'vue';
+import App from './App.vue';
+import KeenUI from 'keen-ui';
+import 'keen-ui/dist/keen-ui.css';
 
-// Vue.use(KeenUI);
+Vue.use(KeenUI);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
